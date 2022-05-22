89fcd037cbdfcd9070cb506631230e8fd27cb2a5
devnulzero <mawuenamichelk@gmail.com>
2022-05-22 23:31:14 +0000

    Add confing.h (Configuration file)

---
+++ config.h
@@ -0,0 +1,29 @@
+// MIT License
+
+// WIFI CREDENTIALS
+#define WIFI_SSID "XXXXXXX"
+#define WIFI_PASS "XXXXXX" 
+
+// HIVEMQ Server
+#define HIVEMQ_SERVER "---------------------------------------------------"
+#define HIVEMQ_PORT 8883
+
+// HIVEMQ CREDENTIALS
+#define HIVEMQ_USER "XXXXXXX"
+#define HIVEMQ_PASSWORD "XXXXXXXXXX"
+
+//Client UID
+#define CLIENT_ID "XX"
+
+//Admin UID
+#define ADMIN_ID "XX"
+
+//Device (POUBELLE)
+#define DEVICE_UID "P-00XXX"
+#define DEVICE_STATUS_TAB {"vide", "semi-plein", "plein", "debordement", "erreur"}
+#define DEVICE_CURRENT_STATUS ""
+#define DEVICE_HAUTEUR 87.5
+
+//GEO Values
+#define LATITUDE_VALUE 6.16060
+#define LONGITUDE_VALUE 1.23385
