69a88c556124b98e2e70e1eb6e92ab2f4f11ea91
devnulzero <mawuenamichelk@gmail.com>
2022-05-26 21:43:25 +0000

    Add Random value config.h

--- config.h
+++ config.h
@@ -1,29 +1,28 @@
 // MIT License
 
-// WIFI CREDENTIALS
-#define WIFI_SSID "XXXXXXX"
-#define WIFI_PASS "XXXXXX" 
+// WIFI CREDENTIALS (USE YOURS)
+#define WIFI_SSID "SSID"
+#define WIFI_PASS "wifipass" 
 
-// HIVEMQ Server
-#define HIVEMQ_SERVER "---------------------------------------------------"
+// HIVEMQ Server (USE YOURS)
+#define HIVEMQ_SERVER "63a31ac110854457a8740f959e1f510a.s1.eu.hivemq.cloud"
 #define HIVEMQ_PORT 8883
 
-// HIVEMQ CREDENTIALS
-#define HIVEMQ_USER "XXXXXXX"
-#define HIVEMQ_PASSWORD "XXXXXXXXXX"
+// HIVEMQ CREDENTIALS (USE YOURS)
+#define HIVEMQ_USER "id"
+#define HIVEMQ_PASSWORD "password"
 
 //Client UID
-#define CLIENT_ID "XX"
+#define CLIENT_ID "13"
 
 //Admin UID
-#define ADMIN_ID "XX"
+#define ADMIN_ID "1"
 
 //Device (POUBELLE)
-#define DEVICE_UID "P-00XXX"
+#define DEVICE_UID "P-00014"
 #define DEVICE_STATUS_TAB {"vide", "semi-plein", "plein", "debordement", "erreur"}
 #define DEVICE_CURRENT_STATUS ""
 #define DEVICE_HAUTEUR 87.5
-
 //GEO Values
 #define LATITUDE_VALUE 6.16060
 #define LONGITUDE_VALUE 1.23385
